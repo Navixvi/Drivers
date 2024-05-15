269597e23297f7009dac6e8cc57fb8b3c62c220a
ivanjavierbell@gmail.com <ivanjavierbell@gmail.com>
2024-05-15 14:34:45 -0300

    DB Models and Relations

--- server/src/models/Driver.js
+++ server/src/models/Driver.js
@@ -1,11 +1,34 @@
 const { DataTypes } = require('sequelize');
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
+
 module.exports = (sequelize) => {
-  // defino el modelo
   sequelize.define('Driver', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
     name: {
       type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    nationality: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    birthDate: {
+      type: DataTypes.DATEONLY,
       allowNull: false,
     },
   });
