9af3536308ff7e89310d6b8fd3480784193ca23e
ivanjavierbell@gmail.com <ivanjavierbell@gmail.com>
2024-05-15 15:10:11 -0300

    GetDriversById controller

---
+++ server/src/conntrollers/driversById.js
@@ -0,0 +1,31 @@
+const axios = require('axios');
+const { Driver } = require('../db');
+
+// Controlador para obtener el detalle de un conductor por ID
+async function getDriverById(req, res) {
+  const driverId = req.params.idDriver;
+
+  try {
+    // Buscar el conductor en la base de datos por ID
+    const driverFromDB = await Driver.findByPk(driverId);
+
+    // Si el conductor se encuentra en la base de datos, devolverlo
+    if (driverFromDB) {
+      return res.json(driverFromDB);
+    }
+
+    // Si el conductor no se encuentra en la base de datos, obtenerlo de la API
+    const response = await axios.get(`http://localhost:5000/drivers/${driverId}`);
+    const driverFromAPI = response.data;
+
+    // Devolver el conductor obtenido de la API
+    res.json(driverFromAPI);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error interno del servidor" });
+  }
+}
+
+module.exports = {
+  getDriverById
+};
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,17 +2,12 @@ const { Router } = require("express");
 const router = Router();
 
 // Importa los controladores necesarios
-const {
-  getDrivers,
-  /*getDriverById,
-  getDriversByName,
-  createDriver,
-  getTeams*/
-} = require("../controllers/driversController");
+const { getDrivers } = require("../conntrollers/driversController");
+const { getDriverById } = require("../conntrollers/driversById");
 
 // Rutas para los conductores
 router.get("/drivers", getDrivers);
-//router.get("/drivers/:idDriver", getDriverById);
+router.get("/drivers/:idDriver", getDriverById);
 //router.get("/drivers/name", getDriversByName);
 //router.post("/drivers", createDriver);
 
