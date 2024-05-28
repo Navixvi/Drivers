ac2669057bc3ece0584ece0b8d47c6fe61b4ebbb
ivanjavierbell@gmail.com <ivanjavierbell@gmail.com>
2024-05-28 10:19:36 -0300

    Teams Filter Fixed

--- server/src/controllers/driversController.js
+++ server/src/controllers/driversController.js
@@ -23,6 +23,7 @@ async function getDrivers(req, res) {
         through: {
           attributes: [],
         },
+        where: {}
       },
       where: {},
     };
@@ -30,27 +31,36 @@ async function getDrivers(req, res) {
     // Filtrar por equipo si se proporciona
     if (team) {
       dbOptions.include.where = { name: team };
+      console.log("Filtrando por equipo:", dbOptions.include.where);
     }
 
     // Filtrar por nacionalidad si se proporciona
     if (nationality) {
       dbOptions.where.nationality = nationality;
+      console.log("Filtrando por nacionalidad:", dbOptions.where.nationality);
     }
+
+    // Verificar opciones de consulta antes de realizar la consulta
+    console.log("Opciones de consulta:", dbOptions);
 
     // Obtener conductores de la base de datos
     const driversFromDB = await Driver.findAll(dbOptions);
+    console.log("Conductores obtenidos de la BD:", driversFromDB);
 
     // Obtener conductores de la API
     const response = await axios.get('http://localhost:5000/drivers');
     let driversFromAPI = response.data;
 
     // Asignar imagen por defecto a los conductores de la API si no tienen una definida
+    const defaultImage = {
+      url: 'https://media.istockphoto.com/id/1087022094/es/foto/piloto-de-carreras.jpg?s=2048x2048&w=is&k=20&c=xMaW128YdFBW-J6v2qp00Ubd7ERiB54_l4McbDgMZZo=',
+      imageby: 'Autor Desconocido'
+    };
+
     driversFromAPI = driversFromAPI.map(driver => ({
       ...driver,
-      image: driver.image || {
-        url: 'https://media.istockphoto.com/id/1087022094/es/foto/piloto-de-carreras.jpg?s=2048x2048&w=is&k=20&c=xMaW128YdFBW-J6v2qp00Ubd7ERiB54_l4McbDgMZZo=',
-        imageby: 'Autor Desconocido',
-      }
+      image: driver.image || defaultImage,
+      teams: driver.teams || []  // Asegurarse de que `teams` esté definido
     }));
 
     // Filtrar conductores de la API por equipo si se proporciona
